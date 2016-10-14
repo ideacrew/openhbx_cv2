7ec234905fbcc3430eb3dafc11a0b813f2392fe1
Trey Evans <lewis.r.evans@gmail.com>
2016-10-14 16:08:13 -0400

    Timestamps to person.

--- lib/openhbx/cv2/individual.rb
+++ lib/openhbx/cv2/individual.rb
@@ -11,6 +11,8 @@ module Openhbx
       element :id, String, tag: "id/cv:id"
       has_one :person, ::Openhbx::Cv2::Person, tag: "person"
       has_one :person_demographics, ::Openhbx::Cv2::PersonDemographics, tag: "person_demographics"
+      element :created_at, Time, tag: "created_at"
+      element :updated_at, Time, tag: "updated_at"
     end
   end
 end
--- spec/openhbx/cv2/individual_spec.rb
+++ spec/openhbx/cv2/individual_spec.rb
@@ -2,6 +2,14 @@ require "spec_helper"
 
 describe Openhbx::Cv2::Individual, "given a sample xml" do
   let(:individual_id) { "Some individual id" }
+  let(:created_at_value) { 
+    dt_value = DateTime.now - 1000
+    Time.mktime(dt_value.year, dt_value.month, dt_value.day, dt_value.hour, dt_value.minute, dt_value.second)
+  }
+  let(:updated_at_value) { 
+    dt_value = DateTime.now
+    Time.mktime(dt_value.year, dt_value.month, dt_value.day, dt_value.hour, dt_value.minute, dt_value.second)
+  }
 
   let(:input_xml) { 
 <<-XMLDOC
@@ -12,6 +20,8 @@ describe Openhbx::Cv2::Individual, "given a sample xml" do
   </id>
   <person />
   <person_demographics />
+  <created_at>#{created_at_value.iso8601}</created_at>
+  <updated_at>#{updated_at_value.iso8601}</updated_at>
 </individual>
 XMLDOC
   }
@@ -28,5 +38,13 @@ XMLDOC
 
   it "has a person_demographics" do
     expect(subject.person_demographics).to be_kind_of Openhbx::Cv2::PersonDemographics
+  end
+
+  it "has the correct updated_at" do
+    expect(subject.updated_at).to eq updated_at_value
+  end
+
+  it "has the correct created_at" do
+    expect(subject.created_at).to eq created_at_value
   end
 end
