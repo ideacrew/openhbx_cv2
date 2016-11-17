89ab4cd86898797245c096a3348296e529e9492d
Trey Evans <lewis.r.evans@gmail.com>
2016-11-17 11:18:17 -0500

    Missing resource id and event_name.

--- lib/openhbx/cv2/enrollment_event_event.rb
+++ lib/openhbx/cv2/enrollment_event_event.rb
@@ -8,6 +8,8 @@ module Openhbx
       tag 'event'
       namespace 'cv'
 
+      element :resource_instance_id, String, xpath: "cv:header/cv:resource_instance_uri/cv:id/cv:id"
+      element :event_name, String, tag: "event_name"
       has_one :body, ::Openhbx::Cv2::EnrollmentEventBody, tag: "body/cv:enrollment_event_body"
     end
   end
--- spec/openhbx/cv2/enrollment_event_event_spec.rb
+++ spec/openhbx/cv2/enrollment_event_event_spec.rb
@@ -1,10 +1,21 @@
 require "spec_helper"
 
 describe Openhbx::Cv2::EnrollmentEventEvent, "given a sample xml" do
+  let(:event_name) { "urn:openhbx:terms:v1:enrollment#initial" }
+  let(:resource_instance_id) { "1234987394" }
+
   let(:input_xml) { 
 <<-XMLDOC
 <?xml version='1.0' encoding='utf-8' ?>
 <event xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://openhbx.org/api/terms/1.0'>
+  <event_name>#{event_name}</event_name>
+  <header>
+    <resource_instance_uri>
+      <id>
+        <id>#{resource_instance_id}</id>
+      </id>
+    </resource_instance_uri>
+  </header>
   <body>
     <enrollment_event_body />
   </body>
@@ -16,5 +27,13 @@ XMLDOC
 
   it "has the correct policy id" do
     expect(subject.body).to be_kind_of Openhbx::Cv2::EnrollmentEventBody
+  end
+
+  it "has the correct enrollment event name" do
+    expect(subject.event_name).to eq event_name
+  end
+
+  it "has the correct resource_instance_id" do
+    expect(subject.resource_instance_id).to eq resource_instance_id
   end
 end
